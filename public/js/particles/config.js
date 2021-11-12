534e45be4a52b336a4cd43bc1e1642a2bd218836
Arczius <70692175+Arczius@users.noreply.github.com>
2021-11-12 09:40:28 +0100

    base particle js imported

---
+++ public/js/particles/config.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    Particles.init({
+        selector: ".background",
+        maxParticles: 1000,
+        color: "#fff",
+        sizeVariations: 6,
+        speed: 9,
+    });
+};
