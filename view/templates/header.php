2ecd1b1a726a7ba690829bbc67b046cec3bb9c7c
Arczius <70692175+Arczius@users.noreply.github.com>
2021-11-12 10:13:15 +0100

    0.01.a: test of bulma werkt

--- view/templates/header.php
+++ view/templates/header.php
@@ -4,13 +4,14 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Studenten app</title>	
-	<link rel="stylesheet" href="<?= URL ?>">
+	<link rel="stylesheet" href="<?= URL ?>public/bulma/css/bulma.css">
+	<link rel="stylesheet" href="<?= URL ?>public/css/style.css">
 </head>
 <body>
 	<nav>
 		<h1>Dit is de coole app van Jesse</h1>
-	<ul>
-		<li><a href="<?= URL ?>home/index">Home</a></li>
-		<li><a href="<?= URL ?>student/index">Students</a></li>
+	<ul class="columns">
+		<li class="column"><a href="<?= URL ?>home/index">Home</a></li>
+		<li class="column"><a href="<?= URL ?>student/index">Students</a></li>
 	</ul>
 	</nav>
